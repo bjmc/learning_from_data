235bdd0e61ea662c2726276979d87c1e502c45fc
Brendan McCollam <bmccollam@uchicago.edu>
2014-10-06 00:10:11 -0500

    Working perceptron, blows up when it converges

--- homework1/problem7.jl
+++ homework1/problem7.jl
@@ -3,12 +3,12 @@ import Distributions
 Uniform = Distributions.Uniform
 
 function rand_points(n)
-    r(i) = rand(Uniform(-1, 1))
-    [ntuple(2, r) for x=1:n]
+    rand!(Uniform(-1, 1), Array(Float64, 2, n))
 end
 
 function get_f()
-    i, j = rand_points(2)
+    points = rand_points(2)
+    i, j = points[:,1], points[:,2]
     m = (j[2] - i[2]) / (j[1] - i[1])
     b = i[2] - (m * i[1])
     line(x) = m*x + b
@@ -26,18 +26,39 @@ end
 function get_points(N)
     f = get_f()
     points = rand_points(N)
-    values = [f(p) for p in points]
+    values = Array(Int64, N)
+    for i = 1:N
+        values[i] = f(points[:,i])
+    end
     return points, values, f
 end
 
-function setup(N):
-    points, values, f = get_points()
+function setup(N)
+    points, values, f = get_points(N)
     w = zeros(3)
     return f, w, points, values
 end
 
-function pla(w, points, values, f)
+function pla(w, points, values)
+    bad = Int64[]
+    for i in 1:size(points, 2)
+        p = points[:,i]
+        g = sign(transpose(w) * [1, p[1], p[2]])[1]
+        y = values[i]
+        if !isequal(g, y)
+            push!(bad, i)
+        end
+    end
+    one_bad = bad[rand(1:size(bad, 1))]
+    y = values[one_bad]
+    bad_point = points[:, one_bad]
+    w = w + (y * [1, bad_point[1], bad_point[2]])
+    return w, bad, one_bad
+end
 
-    miscategorized = sign(transpose(w) * [p[1], p[2], 1]
+function multiple_pla(n_runs, w, points, values)
+    for i = 1:n_runs
+        w = pla(w, points, values)[1]
+    end
 end
 
