78fbd8c96e9c3d1fd2f3a967d9c9bc1776864872
Brendan McCollam <bmccollam@uchicago.edu>
2014-10-06 13:15:21 -0500

    Working code

--- homework1/problem7.jl
+++ homework1/problem7.jl
@@ -49,16 +49,50 @@ function pla(w, points, values)
             push!(bad, i)
         end
     end
-    one_bad = bad[rand(1:size(bad, 1))]
-    y = values[one_bad]
-    bad_point = points[:, one_bad]
-    w = w + (y * [1, bad_point[1], bad_point[2]])
+    one_bad = nothing
+    if length(bad) > 0
+        one_bad = bad[rand(1:size(bad, 1))]
+        y = values[one_bad]
+        bad_point = points[:, one_bad]
+        w = w + (y * [1, bad_point[1], bad_point[2]])
+    end
     return w, bad, one_bad
 end
 
-function multiple_pla(n_runs, w, points, values)
-    for i = 1:n_runs
-        w = pla(w, points, values)[1]
+function multiple_pla(max_runs, w, points, values)
+    i = 0
+    for i = 1:max_runs
+        w, bad, one = pla(w, points, values)
+        if length(bad) == 0
+            break
+        end
     end
+    return w, i
 end
 
+function converges_after(N, n_runs)
+    runs = Array(Int64, n_runs)
+    for i in 1:n_runs
+        f, w, points, values = setup(N)
+        runs[i] = multiple_pla(10000, w, points, values)[2]
+    end
+    mean(runs)
+end
+
+function mismatch_chance(N, n_test, n_points)
+    results = Array(Float64, n_test)
+    for i = 1:n_test
+        temp_results = Array(Bool, n_points)
+        f, w, in_points, values = setup(N)
+        w = multiple_pla(10000, w, in_points, values)[1]
+        g(p) = sign(transpose(w) * [1, p[1], p[2]])[1]
+        out_points = rand_points(n_points)
+        for j = 1:n_points
+            t = out_points[:, j]
+            mismatch = !isequal(f(t), g(t))
+            temp_results[j] = mismatch
+        end
+        results[i] = mean(temp_results)
+    end
+    return mean(results)
+end
